9b6430021ba6226473c47850eb77a6f9f036e504
phaniso <9phaniso9@gmail.com>
2016-02-23 23:56:55 +0100

    Add logs to deployer writable folder list

--- deployer/deploy.php
+++ deployer/deploy.php
@@ -4,13 +4,14 @@ require 'recipe/common.php';
 require '../vendor/deployphp/recipes/recipes/configure.php';
 
 set('keep_releases', 5);
-set('repository', 'https://github.com/phaniso/Multi-Server-Api.git');
-
+set('repository', 'https://github.com/phaniso/phpmonitor-Api.git');
+set('writable_dirs', ['src/logs']);
 task('deploy', [
     'deploy:prepare',
     'deploy:release',
     'deploy:update_code',
     'deploy:shared',
+    'deploy:writable',
     'deploy:vendors',
     'deploy:symlink',
     'cleanup',
